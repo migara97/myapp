dad71c62729e70ae4f0051f83e8b4ce6a676befa
Migara Liyanage <migara@ceylonlinux.lk>
2022-08-22 22:46:49 +0530

    2022_08_22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import FC from "./Components/FunctionalComp"
+import {ClassComp1} from "./Components/ClassComp"
+import Click from "./Components/Click"
+import Counter from "./Components/Counter"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <div>
+      <h1> Hello!Welcome to Simplilearn </h1>
+      <h1>This video is about Componets </h1> 
+      <FC/>
+      {/* <ClassComp/> */}
+      <ClassComp1/>
+      <Click/>
+      <Counter/>
     </div>
-  );
+    );
 }
 
 export default App;
